c6381e721ed23e2ce97d9dd3708bc9c1999a5f5a
hackerchafa <hmgd.14@gmail.com>
2025-01-22 19:31:41 -0600

    Add files via upload

---
+++ prueba6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace prueba6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Escribe un numero para revisar si es primo o no");
+            numero = int.Parse(Console.ReadLine());
+
+            if (EsPrimo(numero))
+            {
+                Console.WriteLine("El numero ingresado si es primo");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado no es primo");
+            }
+            
+            static bool EsPrimo(int numero)
+            {
+                if (numero < 2)
+                {
+                    return false;
+                }
+                
+                for (int i = 2; i < Math.Sqrt(numero); i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
